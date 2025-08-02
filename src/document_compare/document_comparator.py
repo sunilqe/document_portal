308b0b8c0c88f3abfd679dd384584b63444c1758
Sunil Kumar <sunilqe@gmail.com>
2025-08-02 16:16:27 +0530

    data comparartor code updated

--- src/document_compare/document_comparator.py
+++ src/document_compare/document_comparator.py
@@ -1,11 +1,10 @@
-
 import sys
 from dotenv import load_dotenv
 import pandas as pd
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from model.models import *
-from prompt.prompt_library import PROMPT_REGISTRY
+from prompt.prompt_library import PROPMT_REGISTRY
 from utils.model_loader import ModelLoader
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
@@ -18,26 +17,37 @@ class DocumentComparatorLLM:
         self.llm = self.loader.load_llm()
         self.parser = JsonOutputParser(pydantic_object=SummaryResponse)
         self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
-        self.prompt = PROMPT_REGISTRY["document_comparison"]
+        self.prompt = PROPMT_REGISTRY["document_comparison"]
         self.chain = self.prompt | self.llm | self.parser | self.fixing_parser
         self.log.info("DocumentComparatorLLM initialized with model and parser.")
 
-    def compare_documents(self):
+    def compare_documents(self,combined_docs: str) -> pd.DataFrame:
         """
         Compares two documents and returns a structured comparison.
         """
         try:
-            pass
+            inputs = {
+                "combined_docs": combined_docs,
+                "format_instruction": self.parser.get_format_instructions()
+            }
+            self.log.info("Starting document comparison", inputs=inputs)
+            response = self.chain.invoke(inputs)
+            self.log.info("Document comparison completed", response=response)
+            return self._format_response(response)
+
+            
         except Exception as e:
             self.log.error(f"Error in compare_documents: {e}")
             raise DocumentPortalException("An error occurred while comparing documents.",sys)
     
-    def _format_response(self):
+    def _format_response(self,response_parsed: list[dict]) -> pd.DataFrame: #type: ignore
         """
         Formats the response from the LLM into a structured format.
         """
         try:
-            pass
+            df = pd.DataFrame(response_parsed)
+            self.log.info("Response formatted into DataFrame", dataframe=df)
+            return df
         except Exception as e:
             self.log.error("Error formatting response into DataFrame", error=str(e))
             raise DocumentPortalException("Error formatting response",sys)
