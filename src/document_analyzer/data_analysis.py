07619e2be84a19ec8f69625a479ffbb5fd9e0e64
Sunil Kumar <sunilqe@gmail.com>
2025-08-16 15:21:58 +0530

    document analyzer code updated

--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -6,7 +6,7 @@ from exception.custom_exception import DocumentPortalException
 from model.models import *
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
-from prompt.prompt_library import PROMPT_REGISTRY
+from prompt.prompt_library import PROMPT_REGISTRY # type: ignore
 
 class DocumentAnalyzer:
     """
